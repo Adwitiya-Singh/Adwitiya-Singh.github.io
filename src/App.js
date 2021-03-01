94abfd928e046b81ad7bed7f19ead3b0afda06e8
Adwitiya Singh <adwitiya.karan@gmail.com>
2021-03-01 01:55:33 -0800

    Change Colors, add social media links

--- src/App.js
+++ src/App.js
@@ -4,136 +4,144 @@ function App() {
     return (
         <div>
             <div className="titlecard">
-                    ADWITIYA SINGH
+                ADWITIYA SINGH
             </div>
             <div>
-                <img class="center" src="pfp.jpg" alt="Girl in a jacket"/>
+                <img class="center" src="pfp.jpg" alt="Profile Picture"/>
+            </div>
+            <div>
+                <a href="https://www.linkedin.com/in/adwitiya-singh-a31251153/" title="Add me on LinkedIn">
+                    <img class="social" src="linkedin-logo.png" alt="Add me on LinkedIn"/>
+                </a>
+                <a href="https://github.com/Adwitiya-Singh" title="Check out my open source projects">
+                    <img class="social" src="github-logo.png" alt="Check out my open source projects"/>
+                </a>
             </div>
             <div className="about-me">
                 Full Stack Developer at California State University, Northridge & Database Architect at UAW Local 4123
                 Los Angeles, California, United States
-        </div>
-        <div className="timeline-area">
-            <div className="container">
-                <div className="timelines">
-                    <h2>Timeline</h2>
-                </div>
-                <div className="all-timelines">
-                    <div className="single-timeline d-flex-2">
-                        <div className="timeline-blank">
-                            <div className="timeline-text-title">
-                                <h6>April 2019</h6>
+            </div>
+            <div className="timeline-area">
+                <div className="container">
+                    <div className="timelines">
+                        <h2>Timeline</h2>
+                    </div>
+                    <div className="all-timelines">
+                        <div className="single-timeline d-flex-2">
+                            <div className="timeline-blank">
+                                <div className="timeline-text-title">
+                                    <h6>April 2019</h6>
+                                </div>
                             </div>
-                        </div>
-                        <div className="timeline-text d-flex-2">
+                            <div className="timeline-text d-flex-2">
 					<span>
 						The launch of Aryancoin; AYA ICO, resulting in over 30BTC collected
 					</span>
-                            <div className="t-square"></div>
-                        </div>
-                    </div>
-                    <div className="single-timeline d-flex-2">
-                        <div className="timeline-blank">
-                            <div className="timeline-text-title">
-                                <h6>December 2019 </h6>
+                                <div className="t-square"></div>
                             </div>
                         </div>
-                        <div className="timeline-text d-flex-2">
+                        <div className="single-timeline d-flex-2">
+                            <div className="timeline-blank">
+                                <div className="timeline-text-title">
+                                    <h6>December 2019 </h6>
+                                </div>
+                            </div>
+                            <div className="timeline-text d-flex-2">
 					<span>
 						The launch of AYA Pay
 					</span>
-                            <div className="t-square"></div>
-                        </div>
-                    </div>
-                    <div className="single-timeline d-flex-2">
-                        <div className="timeline-blank">
-                            <div className="timeline-text-title">
-                                <h6>April 2020</h6>
+                                <div className="t-square"></div>
                             </div>
                         </div>
-                        <div className="timeline-text d-flex-2">
+                        <div className="single-timeline d-flex-2">
+                            <div className="timeline-blank">
+                                <div className="timeline-text-title">
+                                    <h6>April 2020</h6>
+                                </div>
+                            </div>
+                            <div className="timeline-text d-flex-2">
 					<span>
 						The successful Hamedan Hardfork, supported by all AYA exchanges, aimed at integrating the dPoW algorithm, improving the security of the AYA blockchain.
 					</span>
-                            <div className="t-square"></div>
-                        </div>
-                    </div>
-                    <div className="single-timeline d-flex-2">
-                        <div className="timeline-blank">
-                            <div className="timeline-text-title">
-                                <h6>July 2020</h6>
+                                <div className="t-square"></div>
                             </div>
                         </div>
-                        <div className="timeline-text d-flex-2">
+                        <div className="single-timeline d-flex-2">
+                            <div className="timeline-blank">
+                                <div className="timeline-text-title">
+                                    <h6>July 2020</h6>
+                                </div>
+                            </div>
+                            <div className="timeline-text d-flex-2">
 					<span>
 						Aryna Exchange goes live, opening more trading opportunities globally
 						<img src="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"
                              alt="Girl in a jacket"/>
 					</span>
-                            <div className="t-square"></div>
-                        </div>
-                    </div>
-                    <div className="single-timeline d-flex-2">
-                        <div className="timeline-blank">
-                            <div className="timeline-text-title">
-                                <h6>July 2020</h6>
+                                <div className="t-square"></div>
                             </div>
                         </div>
-                        <div className="timeline-text d-flex-2">
+                        <div className="single-timeline d-flex-2">
+                            <div className="timeline-blank">
+                                <div className="timeline-text-title">
+                                    <h6>July 2020</h6>
+                                </div>
+                            </div>
+                            <div className="timeline-text d-flex-2">
 					<span>
 						The enabling of our Coin Exchanger
 					</span>
-                            <div className="t-square"></div>
-                        </div>
-                    </div>
-
-                    <div className="single-timeline d-flex-2">
-                        <div className="timeline-blank">
-                            <div className="timeline-text-title">
-                                <h6>June 2020</h6>
+                                <div className="t-square"></div>
                             </div>
                         </div>
-                        <div className="timeline-text d-flex-2">
+
+                        <div className="single-timeline d-flex-2">
+                            <div className="timeline-blank">
+                                <div className="timeline-text-title">
+                                    <h6>June 2020</h6>
+                                </div>
+                            </div>
+                            <div className="timeline-text d-flex-2">
 					<span>
 						The implementation of Smart Contracts into the Aryacoin Ecosystem
 					</span>
-                            <div className="t-square"></div>
-                        </div>
-                    </div>
-                    <div className="single-timeline d-flex-2">
-                        <div className="timeline-blank">
-                            <div className="timeline-text-title">
-                                <h6>Q1 2021</h6>
+                                <div className="t-square"></div>
                             </div>
                         </div>
-                        <div className="timeline-text d-flex-2">
+                        <div className="single-timeline d-flex-2">
+                            <div className="timeline-blank">
+                                <div className="timeline-text-title">
+                                    <h6>Q1 2021</h6>
+                                </div>
+                            </div>
+                            <div className="timeline-text d-flex-2">
 					<span>
 						Alef B goes live (more details coming soon)
 						<img src="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"
                              alt="Girl in a jacket"/>
 					</span>
-                            <div className="t-square"></div>
-                        </div>
-                    </div>
-                    <div className="single-timeline d-flex-2">
-                        <div className="timeline-blank">
-                            <div className="timeline-text-title">
-                                <h6>Q1 2021</h6>
+                                <div className="t-square"></div>
                             </div>
                         </div>
-                        <div className="timeline-text d-flex-2">
+                        <div className="single-timeline d-flex-2">
+                            <div className="timeline-blank">
+                                <div className="timeline-text-title">
+                                    <h6>Q1 2021</h6>
+                                </div>
+                            </div>
+                            <div className="timeline-text d-flex-2">
 					<span>
 						Alef B goes live (more details coming soon)
 						<img src="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"
                              alt="Girl in a jacket"/>
 					</span>
-                            <div className="t-square"></div>
+                                <div className="t-square"></div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-</div>
     );
 }
 
