39d9aff8c5f6174cd60ce637b49e9196f885f9a9
Adwitiya Singh <adwitiya.karan@gmail.com>
2021-03-02 05:39:19 -0800

    Add scroll defined header bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,62 @@
 import './App.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin, faGithubSquare } from '@fortawesome/free-brands-svg-icons'
-import { faFileAlt, faMapPin } from '@fortawesome/free-solid-svg-icons'
+import { faFileAlt, faMapPin, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import React, {useState} from 'react'
 
 function App() {
     const [iconColor] = useState("#101820FF");
+    if (
+        "IntersectionObserver" in window &&
+        "IntersectionObserverEntry" in window &&
+        "intersectionRatio" in window.IntersectionObserverEntry.prototype
+    ) {
+        let observer = new IntersectionObserver(entries => {
+            if (entries[0].boundingClientRect.y < 0) {
+                document.body.classList.add("header-not-at-top");
+            } else {
+                document.body.classList.remove("header-not-at-top");
+            }
+        });
+        observer.observe(document.querySelector("#top-of-site-pixel-anchor"));
+    }
     return (
         <div>
+            <header>
+                <div>
+                    <ul className="c-social">
+                        <li>
+                            <a href="mailto:adwitiya.karan@gmail.com" title="Add me on LinkedIn">
+                                <FontAwesomeIcon icon={faEnvelope} color={iconColor}/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/adwitiya-singh-a31251153/" title="Add me on LinkedIn">
+                                <FontAwesomeIcon icon={faLinkedin} color={iconColor}/>
+                            </a>
+                        </li>
+                        <li>
+                                &nbsp; AS &nbsp;
+                        </li>
+                        <li>
+                            <a href="resume.pdf" title="Check out my resume">
+                                <FontAwesomeIcon icon={faFileAlt} color={iconColor}/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/Adwitiya-Singh" title="Check out my projects">
+                                <FontAwesomeIcon icon={faGithubSquare} color={iconColor}/>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </header>
+            <body>
             <div className="titlecard">
                 ADWITIYA SINGH
             </div>
             <div>
-                <img className="center" src="pfp.jpg" alt="Profile Picture"/>
+                <img className="center" src="pfp.jpg" alt="Profile"/>
             </div>
             <div className="map-pin">
                 <FontAwesomeIcon icon={faMapPin}color={iconColor}/>
@@ -20,6 +64,11 @@ function App() {
             </div>
             <div>
                 <ul className="c-social">
+                    <li>
+                        <a href="mailto:adwitiya.karan@gmail.com" title="Add me on LinkedIn">
+                            <FontAwesomeIcon icon={faEnvelope} color={iconColor}/>
+                        </a>
+                    </li>
                     <li>
                         <a href="https://www.linkedin.com/in/adwitiya-singh-a31251153/" title="Add me on LinkedIn">
                             <FontAwesomeIcon icon={faLinkedin} color={iconColor}/>
@@ -50,12 +99,18 @@ function App() {
                         <div className="single-timeline d-flex-2">
                             <div className="timeline-blank">
                                 <div className="timeline-text-title">
-                                    <h6>April 2019</h6>
+                                    <h6>July 2019</h6>
                                 </div>
                             </div>
                             <div className="timeline-text d-flex-2">
 					<span>
-						The launch of Aryancoin; AYA ICO, resulting in over 30BTC collected
+						Full Stack Developer - CSU, Northridge <br />
+                        · Worked on the frontend and backend for the content management system for our campus chat bot. Tools used include:<br />
+                        &nbsp; - RESTful API in python using SQLAlchemy as the ORM hosted on an AWS Lambda for the Backend<br />
+                        &nbsp; - React.js interface for the Frontend with the API library generated using OpenAPI<br />
+                        &nbsp; - Dialogflow integration for seamless training of the chatbot<br />
+                        · Wrote accessible components for our, heavily accessibility focused, in-house React Component library<br />
+
 					</span>
                                 <div className="t-square"></div>
                             </div>
@@ -63,12 +118,15 @@ function App() {
                         <div className="single-timeline d-flex-2">
                             <div className="timeline-blank">
                                 <div className="timeline-text-title">
-                                    <h6>December 2019 </h6>
+                                    <h6>February 2018</h6>
                                 </div>
                             </div>
                             <div className="timeline-text d-flex-2">
 					<span>
-						The launch of AYA Pay
+						Instructional Student Assistant - CSU, Northridge<br />
+						· Reviewed computer science class material specifically with implementation of data structures and algorithms with students
+                        to help them discover the correct answers to problems in Java<br />
+                        · Conducted tutorial services for students to provide academic support in computer programming
 					</span>
                                 <div className="t-square"></div>
                             </div>
@@ -95,8 +153,6 @@ function App() {
                             <div className="timeline-text d-flex-2">
 					<span>
 						Aryna Exchange goes live, opening more trading opportunities globally
-						<img src="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"
-                             alt="Girl in a jacket"/>
 					</span>
                                 <div className="t-square"></div>
                             </div>
@@ -135,11 +191,41 @@ function App() {
                                 </div>
                             </div>
                             <div className="timeline-text d-flex-2">
-					<span>
-						Alef B goes live (more details coming soon)
-						<img src="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"
-                             alt="Girl in a jacket"/>
-					</span>
+                                    <span>
+                                        Alef B goes live (more details coming soon)
+                                        <img src="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"
+                                             alt="Girl in a jacket"/>
+                                    </span>
+                                <div className="t-square"></div>
+                            </div>
+                        </div>
+                        <div className="single-timeline d-flex-2">
+                            <div className="timeline-blank">
+                                <div className="timeline-text-title">
+                                    <h6>Q1 2021</h6>
+                                </div>
+                            </div>
+                            <div className="timeline-text d-flex-2">
+                                    <span>
+                                        Alef B goes live (more details coming soon)
+                                        <img src="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"
+                                             alt="Girl in a jacket"/>
+                                    </span>
+                                <div className="t-square"></div>
+                            </div>
+                        </div>
+                        <div className="single-timeline d-flex-2">
+                            <div className="timeline-blank">
+                                <div className="timeline-text-title">
+                                    <h6>Q1 2021</h6>
+                                </div>
+                            </div>
+                            <div className="timeline-text d-flex-2">
+                                    <span>
+                                        Alef B goes live (more details coming soon)
+                                        <img src="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"
+                                             alt="Girl in a jacket"/>
+                                    </span>
                                 <div className="t-square"></div>
                             </div>
                         </div>
@@ -152,8 +238,6 @@ function App() {
                             <div className="timeline-text d-flex-2">
 					<span>
 						Alef B goes live (more details coming soon)
-						<img src="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"
-                             alt="Girl in a jacket"/>
 					</span>
                                 <div className="t-square"></div>
                             </div>
@@ -164,6 +248,8 @@ function App() {
             <div>
                 copyright © 2021
             </div>
+            </body>
+            <div id="top-of-site-pixel-anchor"></div>
         </div>
     );
 }
